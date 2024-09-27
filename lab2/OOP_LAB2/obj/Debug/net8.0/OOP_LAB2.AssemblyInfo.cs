e9998c98bdc22523ab4f51bdcb681e47685e6a2f
ironsast <rustknight7@mail.ru>
2024-09-27 12:52:14 +0300

    Update README and add report dir

--- lab2/OOP_LAB2/obj/Debug/net8.0/OOP_LAB2.AssemblyInfo.cs
+++ lab2/OOP_LAB2/obj/Debug/net8.0/OOP_LAB2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_LAB2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a424b227815e64f67693755c18146108954b90fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_LAB2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_LAB2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
